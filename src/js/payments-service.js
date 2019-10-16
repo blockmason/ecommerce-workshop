b2138e69a9e1cc61f4f9333bf20eca6e5b8b9953
Clayton Savage <clytnsavage@gmail.com>
2019-10-16 15:58:40 -0700

    removed try catch in paymentService

--- src/js/payments-service.js
+++ src/js/payments-service.js
@@ -3,36 +3,30 @@ require('dotenv').config();
 
 // Ethereum Ropsten API access
 const paymentMicroservice = link({
-    clientId: process.env.PAYMENT_CLIENT_ID,
-    clientSecret: process.env.PAYMENT_CLIENT_SECRET
+  clientId: process.env.PAYMENT_CLIENT_ID,
+  clientSecret: process.env.PAYMENT_CLIENT_SECRET
 });
 
 const paymentService = {
 
-    balanceOf: async function (address) {
-        const reqBody = {
-            "_tokenholder": address
-        }
-        const { balance } = await paymentMicroservice.get('/balanceOf', reqBody);
-        return(parseInt(balance, 16) / Math.pow(10, 18));
-    },
+  balanceOf: async function (address) {
+    const reqBody = {
+      "_tokenholder": address
+    }
+    const { balance } = await paymentMicroservice.get('/balanceOf', reqBody);
+    return (parseInt(balance, 16) / Math.pow(10, 18));
+  },
 
-    transferFrom: async function transferFunds(sender, recipient, amount) {
-        const funds = (amount*Math.pow(10, 18)).toString(16);
-        const transferBody = {
-          "_from": sender,
-          "_to": recipient,
-          "_value": funds
-        }
-        try {
-          console.log('transfering funds now');
-          await paymentMicroservice.post('/transferFrom', transferBody);
-          console.log('transferring funds complete');
-        }
-        catch(err) {
-          console.log(err);
-        }
-       },
+  transferFrom: async function transferFunds(sender, recipient, amount) {
+    const funds = (amount * Math.pow(10, 18)).toString(16);
+    const transferBody = {
+      "_from": sender,
+      "_to": recipient,
+      "_value": funds
+    }
+    await paymentMicroservice.post('/transferFrom', transferBody);
+    console.log('transferring funds complete');
+  },
 }
 
 module.exports = paymentService;
